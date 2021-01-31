8970b14578a8d4601f8e05198806960ed78d7ae0
Joe Wu <wuzongsheng@gmail.com>
2021-01-31 21:47:14 +0800

    apply toolchain version header

--- unittest/ut_version.cpp
+++ unittest/ut_version.cpp
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
+#include <meta/info.hpp>
 
-namespace miu::project {
-extern std::string_view version();
-extern std::string_view build_info();
-}    // namespace miu::project
+#include "project/version.hpp"
 
 TEST(ut_version, version) {
     std::cout << miu::project::version() << std::endl;
     std::cout << miu::project::build_info() << std::endl;
+
+    std::cout << miu::meta::info() << std::endl;
 }
